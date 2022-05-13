e435063da93f4d1da94c588168740f932c8246a7
Daniel Ellis <up940148@myport.ac.uk>
2022-05-13 13:00:49 +0100

    Wheel of Fortune V1 complete

---
+++ summer-challenges/6_wheel-of-fortune/fortune-wheel.js
@@ -0,0 +1,101 @@
+let options = [
+  {name: 'Test A', colour: '#ff0000'},
+  {name: 'Test B', colour: '#0ff000'},
+  {name: 'Test C', colour: '#00ff00'},
+  {name: 'Test D', colour: '#000ff0'},
+  {name: 'Test E', colour: '#0000ff'},
+  {name: 'Test F', colour: '#f0000f'},
+]
+let currentRotation = 0;
+
+function loadColours() {
+  const saved = localStorage.getItem('options');
+  if (saved) {
+    options = JSON.parse(saved);
+  } else {
+    localStorage.setItem('options', JSON.stringify(options));
+    loadColours();
+    return;
+  }
+}
+
+function loadPicker() {
+  loadColours();
+  const container = document.getElementById('optionChanger');
+  for (let i = 0; i < options.length; i++) {
+    const thisOption = document.createElement('div');
+    thisOption.classList.add('colour-option');
+
+    const optionName = document.createElement('input');
+    optionName.classList.add('option-name');
+    optionName.type = 'text';
+    optionName.value = options[i].name;
+    optionName.addEventListener('change', () => {
+      options[i].name = optionName.value;
+      localStorage.setItem('options', JSON.stringify(options));
+      loadWheel();
+    })
+
+    const optionColour = document.createElement('input');
+    optionColour.classList.add('option-colour');
+    optionColour.type = 'color';
+    optionColour.value = options[i].colour;
+    optionColour.addEventListener('change', () => {
+      options[i].colour = optionColour.value;
+      localStorage.setItem('options', JSON.stringify(options));
+      loadWheel();
+    })
+
+    thisOption.appendChild(optionName);
+    thisOption.appendChild(optionColour);
+    container.appendChild(thisOption);
+  }
+}
+
+function loadWheel() {
+  const canvContainer = document.getElementById('wheelContainer');
+  // Create pointer
+  const pointer = document.getElementById('pointer');
+  const pointerCtx = pointer.getContext('2d');
+  pointerCtx.rect(pointer.width/2 - 2, 0, 4, 50);
+  pointerCtx.strokeStyle = 'white';
+  pointerCtx.fillStyle = 'black';
+  pointerCtx.fill();
+  pointerCtx.stroke();
+
+  // Create wheel stuff
+  const wheel = document.getElementById('wheelCanv');
+  const ctx = wheel.getContext('2d');
+  const segments = options.length;
+  const arc = 2 * Math.PI / segments;
+  for (let i = 0; i < segments; i++) {
+    // Create segment
+    ctx.beginPath();
+    ctx.fillStyle = options[i].colour;
+    ctx.arc(wheel.width / 2, wheel.height / 2, wheel.width * 0.49, arc * i, arc * (i+1));
+    ctx.lineTo(wheel.width / 2, wheel.height / 2);
+    ctx.fill();
+  }
+  ctx.textAlign = 'right';
+  ctx.fillStyle = '#000000';
+  ctx.strokeStyle = '#ffffff';
+  ctx.font = 'bold 48px calibri';
+  for (let i = 0; i < segments; i++) {
+    const angle = arc * (i + 0.5);
+    ctx.save();
+    ctx.beginPath();
+    ctx.translate(wheel.width/2, wheel.width/2);
+    ctx.rotate(angle);
+    ctx.fillText(options[i].name, 4*wheel.width/9, 16);
+    ctx.strokeText(options[i].name, 4*wheel.width/9, 16);
+    ctx.restore();
+  }
+}
+
+document.getElementById('spinWheel').addEventListener('click', () => {
+  currentRotation += Math.random() * 360 + 1440;
+  document.getElementById('wheelCanv').style.transform = `rotate(${currentRotation}deg)`;
+})
+
+loadPicker();
+loadWheel();
